4088bcdb79419d8add3043f0117ab4288883b7bc
MatthiasMimault <34157729+MatthiasMimault@users.noreply.github.com>
2019-10-09 11:37:27 +0100

    Timer fix

--- src/source/JSphSolidCpu_M.cpp
+++ src/source/JSphSolidCpu_M.cpp
@@ -10455,7 +10455,7 @@ template<bool shift> void JSphSolidCpu::ComputeSymplecticCorrT_CompressBdy_M(dou
 		// #Mobile #Bdy
 		// Give a velocity to the compressive part only during a certain time (5000 s for 0.02 mm)
 		// Give a velocity to the compressive part only during a certain time (2500 s for 0.01 mm)
-		if (Posc[p].x > 0.0 && TimeStep < 5000.0) {
+		if (Posc[p].x > 0.0 && TimeStep < 2500.0) {
 			Velrhopc[p] = TFloat4(-0.01f / 2500.0f, 0, 0, (rhopnew < RhopZero ? RhopZero : rhopnew));
 			double dx = double(VelrhopPrec[p].x) * dt05;
 			double dy = double(VelrhopPrec[p].y) * dt05;
